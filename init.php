14e21b4a1933d7a0fdd9489b6e21c703115e145c
Colin Leung <colin@digi3studio.com>
2017-09-06 16:32:10 +0800

    add config files

--- init.php
+++ init.php
@@ -4,10 +4,12 @@ use \Kohana\HTTP\Header as Header;
 $config = new Config();
 $config->attach(new Config_File());
 
-$mimes = $config->load('mimes');
-$userAgents = $config->load('user_agents');
-
-Header::$mimes = $mimes;
-Text::$browsers = $userAgents->get('browser');
-
+/* Warning!!! this add a lot dependency to config... */
+Header::$mimes = $config->load('mimes');
+File::$mimes = $config->load('mimes');
+Text::$browsers = $config->load('user_agents')->get('browser');
+Session::$config = $config->load('session')->as_array();
+Encrypt::$config = $config->load('encrypt')->as_array();
+Inflector::$config = $config->load('inflector')->as_array();
+Valid::$credit_cards = $config->load('credit_cards')->as_array();
 //Text::group = Kohana::$config->load('user_agents')->$value
